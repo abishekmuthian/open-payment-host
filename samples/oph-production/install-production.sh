da3f7befca31ce4e2fa8867421654f8e596bd025
abishek <heavyinfo@gmail.com>
2022-12-04 00:05:49 +0530

    Production dockerfile has lesser volumes and the install script reflects that

--- samples/oph-production/install-production.sh
+++ samples/oph-production/install-production.sh
@@ -4,6 +4,8 @@ set -eu
 # Open Payment Host production setup using `docker-compose`.
 # See https://github.com/abishekmuthian/open-payment-host for detailed installation steps.
 
+alias up="cd .."
+
 check_dependencies() {
 	if ! command -v curl > /dev/null; then
 		echo "curl is not installed."
@@ -26,13 +28,20 @@ setup_folders(){
 	mkdir db
 	mkdir log
 	mkdir certs
-	mkdir --p public/assets/icons
-	mkdir --p public/assets/images/app
-	mkdir --p public/assets/images/products
-}
-
-setup_env(){
-	curl -o fragmenta.env https://raw.githubusercontent.com/abishekmuthian/open-payment-host/main/samples/oph-production/fragmenta.env
+	mkdir public
+	cd public
+	mkdir assets
+	cd  assets 
+	mkdir icons
+	mkdir images
+	cd images
+	mkdir app
+	mkdir products
+	cd -
+	mkdir scripts
+	mkdir styles
+	up
+	up
 }
 
 setup_db(){
@@ -41,27 +50,18 @@ setup_db(){
     cd -
 }
 
-setup_images(){
-	curl -o favicon.ico https://raw.githubusercontent.com/abishekmuthian/open-payment-host/blob/main/public/favicon.ico
-    cd public/assets/images/app
-	curl -o favicon.ico https://raw.githubusercontent.com/abishekmuthian/open-payment-host/blob/main/public/assets/images/app/oph_featured_image.png
-    cd -
-}
-
 setup_containers() {
 	curl -o docker-compose.yml https://raw.githubusercontent.com/abishekmuthian/open-payment-host/main/samples/oph-production/docker-compose.yml
-	docker-compose --env-file fragmenta.env up -d
+	docker-compose up -d
 }
 
 show_output(){
-	echo -e "\nOpen Payment Host is now up and running. Visit http://localhost:443 in your browser.\n"
+	echo -e "\nOpen Payment Host is now up and running. Stop the container, Set the required production configuration and re-run the container.\n"
 }
 
 
 check_dependencies
 setup_folders
-setup_env
 setup_db
-setup_images
 setup_containers
 show_output
